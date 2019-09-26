2c698073d0ee7091054fa33938d6678b515430c1
Vinoth Energetic <vinothcse2000@gmail.com>
2019-09-26 13:06:23 +0530

    Create V6LinuxInit.sh

---
+++ ShellScript/V6LinuxInit.sh
@@ -0,0 +1,19 @@
+alias c=clear
+alias ..="cd ../"
+export PS1='\u@\h: '
+alias ll='ls -hltr'
+alias ebash='vi /home/nrspapp/v6.bash'
+
+#git
+alias gs='git status'
+alias gb='git branch'
+alias gba='git branch -a'
+
+
+#Project Specific
+
+
+#TEMP
+
+
+ 
