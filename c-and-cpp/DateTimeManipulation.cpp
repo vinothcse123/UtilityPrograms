e08555c5fd73ab21d2719e171056c7e5ff173e8c
Vinoth Energetic <vinothcse2000@gmail.com>
2019-09-30 13:38:03 +0530

    Update DateTimeManipulation.cpp

--- c-and-cpp/DateTimeManipulation.cpp
+++ c-and-cpp/DateTimeManipulation.cpp
@@ -8,11 +8,14 @@ class DateTimeManipulation
 {
 	time_t m_rawTime;
 
-
-
 public:
 
 	DateTimeManipulation()
+	{
+		m_rawTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	}
+
+	void resetTime()
 	{
 		m_rawTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	}
