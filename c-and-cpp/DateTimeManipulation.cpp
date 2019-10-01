1aa58af27a848634820f57b5970652a791972815
Vinoth Energetic <vinothcse2000@gmail.com>
2019-10-01 18:40:12 +0530

    Update DateTimeManipulation.cpp

--- c-and-cpp/DateTimeManipulation.cpp
+++ c-and-cpp/DateTimeManipulation.cpp
@@ -24,6 +24,15 @@ public:
 	{
 		std::cout << ctime(&m_rawTime) << std::endl;
 	}
+
+	std::string printDateTimeWithUnderscore()
+	{
+		struct tm * timeinfo;
+		timeinfo = localtime(&m_rawTime);
+		char buffer[80];
+		strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
+		return std::string(buffer);
+	}
 };
 
 int main()
