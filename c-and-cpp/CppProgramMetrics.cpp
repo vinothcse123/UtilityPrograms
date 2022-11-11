7767a7afc8c8eff85af8c8d03ee15b27a7bf4e0c
Vinoth Energetic <vinothcse2000@gmail.com>
2022-11-11 13:23:21 +0530

    Update CppProgramMetrics.cpp

--- c-and-cpp/CppProgramMetrics.cpp
+++ c-and-cpp/CppProgramMetrics.cpp
@@ -7,9 +7,15 @@
 
 /* ================ Start of Metrics measurement code ==================*/
 
+#define ELAPSED_TIME_B(myArgument) ElapsedTime myArgument;myArgument.start(std::string(__FUNCTION__)+std::string(" : ")+std::string(#myArgument));
+
+#define ELAPSED_TIME_E(myArgument) myArgument.stop();
+
+
+
 /*
 Currently it is supported for linux, for others os it is not supported.
-In order for easy copying to measure metrics, all class definitions are handled in single file 
+In order for easy copying to measure metrics, all class definitions are handled in single file
 */
 
 ///@brief Computation of elapsed time of program
@@ -19,18 +25,17 @@ private:
     ///@brief Message for elapsed time
     std::string m_startMsg;
 
-     ///@brief   convertToReqUnit
+    ///@brief   convertToReqUnit
     ///@return   None
     double convertToReqUnit()
     {
-        switch(m_unit)
+        switch (m_unit)
         {
-            case Unit_t::SECONDS:
+        case Unit_t::SECONDS:
             return m_elapsedTime;
 
-            case Unit_t::MILLISECONDS:
-            return m_elapsedTime*1000;
-
+        case Unit_t::MILLISECONDS:
+            return m_elapsedTime * 1000;
         }
 
         return -1;
@@ -38,27 +43,25 @@ private:
 
     std::string getUnitDesc()
     {
-        switch(m_unit)
+        switch (m_unit)
         {
-            case Unit_t::SECONDS:
+        case Unit_t::SECONDS:
             return "s";
 
-            case Unit_t::MILLISECONDS:
+        case Unit_t::MILLISECONDS:
             return "ms";
-
         }
 
         return "INVALID UNIT";
     }
 
 public:
-
     ///@brief time point which holds the start and end time
     std::chrono::time_point<std::chrono::steady_clock> begin, end;
 
     enum class Unit_t
     {
-        SECONDS=0,
+        SECONDS = 0,
         MILLISECONDS,
     };
 
@@ -66,10 +69,7 @@ public:
     double m_elapsedTime;
 
     ///@brief unit
-    Unit_t m_unit=Unit_t::MILLISECONDS;
-
-    
-
+    Unit_t m_unit = Unit_t::MILLISECONDS;
 
     ///@brief    start capturing elapsed time
     ///@return   None
@@ -119,7 +119,7 @@ public:
         std::chrono::duration<double> timeTaken = end - begin;
 
         m_elapsedTime += timeTaken.count();
-        
+
         printTime();
 
         return convertToReqUnit();
@@ -132,7 +132,6 @@ public:
         end = std::chrono::steady_clock::now();
         std::chrono::duration<double> timeTaken = end - begin;
         m_elapsedTime += timeTaken.count();
-
     }
 
     ///@brief    destructor
@@ -144,7 +143,7 @@ public:
 class CppMemoryMetrics
 {
 public:
-    //Returns the peak memory usage so far in bytes
+    // Returns the peak memory usage so far in bytes
     size_t getPeakMemoryUsageInBytes()
     {
         struct rusage rusage;
@@ -155,7 +154,7 @@ public:
 
     void printMemoryUsageInKb()
     {
-        //V6P - change to macro
+        // V6P - change to macro
         std::cout << "Memory usage(KB): " << getPeakMemoryUsageInBytes() / 1024 << std::endl;
     }
 };
@@ -164,48 +163,46 @@ public:
 class CppCpuMetrics
 {
 private:
-    ///@brief resource usage 
+    ///@brief resource usage
     struct rusage cpuResUsage;
 
     ///@brief start and end time of cpu
-    double  m_endCpuTime;
+    double m_endCpuTime;
 
     ///@brief is thread cpu time
-    bool  m_isThreadCpuTime=false;
+    bool m_isThreadCpuTime = false;
 
     ///@brief total cpu time
-    long double  m_totalCpuTime;
+    long double m_totalCpuTime;
 
     ///@brief starting user cpu time
-    long double  m_startUserCpuTime=0;
+    long double m_startUserCpuTime = 0;
 
     ///@brief starting system cpu time
-    long double  m_startSysCpuTime=0;
+    long double m_startSysCpuTime = 0;
 
     ///@brief user cpu time
-    long double  m_userCpuTime=0;
+    long double m_userCpuTime = 0;
 
     ///@brief  system cpu time
-    long double  m_sysCpuTime=0;
-
+    long double m_sysCpuTime = 0;
 
     ///@brief message to display
     std::string m_descriptionStr;
 
-    const int numberOfMicroSecInSec=1000000;
+    const int numberOfMicroSecInSec = 1000000;
 
-     ///@brief   convertToReqUnit
+    ///@brief   convertToReqUnit
     ///@return   None
-    long double convertToReqUnit(const long double& dNumber)
+    long double convertToReqUnit(const long double &dNumber)
     {
-        switch(m_unit)
+        switch (m_unit)
         {
-            case Unit_t::SECONDS:
+        case Unit_t::SECONDS:
             return dNumber;
 
-            case Unit_t::MILLISECONDS:
-            return dNumber*1000;
-
+        case Unit_t::MILLISECONDS:
+            return dNumber * 1000;
         }
 
         return -1;
@@ -213,14 +210,13 @@ private:
 
     std::string getUnitDesc()
     {
-        switch(m_unit)
+        switch (m_unit)
         {
-            case Unit_t::SECONDS:
+        case Unit_t::SECONDS:
             return "s";
 
-            case Unit_t::MILLISECONDS:
+        case Unit_t::MILLISECONDS:
             return "ms";
-
         }
 
         return "INVALID UNIT";
@@ -230,30 +226,27 @@ public:
     ///@brief    constructor
     ///@isThreadCpu - In case of multithreading and if we want to measure CPU consumed by current thread alone this has to be set as true
     ///@return   None
-    CppCpuMetrics(const bool& isThreadCpu=false):
-    m_isThreadCpuTime(isThreadCpu)
+    CppCpuMetrics(const bool &isThreadCpu = false) : m_isThreadCpuTime(isThreadCpu)
     {
         m_endCpuTime = 0;
         m_totalCpuTime = 0;
     }
 
-    
-enum class Unit_t
+    enum class Unit_t
     {
-        SECONDS=0,
+        SECONDS = 0,
         MILLISECONDS,
     };
 
     ///@brief unit
-    Unit_t m_unit=Unit_t::MILLISECONDS;
-
+    Unit_t m_unit = Unit_t::MILLISECONDS;
 
     ///@brief    Returns user cpu time from beginning of program to this point
     ///@return   double
     double getUserCpuTimeInSeconds()
-    {        
-        getrusage(m_isThreadCpuTime? RUSAGE_THREAD  : RUSAGE_SELF, &cpuResUsage);
-        //tv_usec is microseconds, so dividing by 1000000
+    {
+        getrusage(m_isThreadCpuTime ? RUSAGE_THREAD : RUSAGE_SELF, &cpuResUsage);
+        // tv_usec is microseconds, so dividing by 1000000
         m_userCpuTime = convertToReqUnit(cpuResUsage.ru_utime.tv_sec + (cpuResUsage.ru_utime.tv_usec / static_cast<double>(numberOfMicroSecInSec))) - m_startUserCpuTime;
 
         return m_userCpuTime;
@@ -271,9 +264,9 @@ enum class Unit_t
     double getSystemCpuTimeInSeconds()
     {
 
-        getrusage(m_isThreadCpuTime? RUSAGE_THREAD : RUSAGE_SELF, &cpuResUsage);
-        //tv_usec is microseconds, so dividing by 1000000
-        m_sysCpuTime =  convertToReqUnit(cpuResUsage.ru_stime.tv_sec + (cpuResUsage.ru_stime.tv_usec / static_cast<double>(numberOfMicroSecInSec))) - m_startSysCpuTime;
+        getrusage(m_isThreadCpuTime ? RUSAGE_THREAD : RUSAGE_SELF, &cpuResUsage);
+        // tv_usec is microseconds, so dividing by 1000000
+        m_sysCpuTime = convertToReqUnit(cpuResUsage.ru_stime.tv_sec + (cpuResUsage.ru_stime.tv_usec / static_cast<double>(numberOfMicroSecInSec))) - m_startSysCpuTime;
 
         return m_sysCpuTime;
     }
@@ -288,16 +281,15 @@ enum class Unit_t
     ///@brief    Starts the cpu time when this function is called
     ///@param descriptionStr : descriptionStr
     ///@return   None
-    void start(const std::string &descriptionStr="")
+    void start(const std::string &descriptionStr = "")
     {
-        m_startUserCpuTime=getUserCpuTimeInSeconds();
-        m_startSysCpuTime=getSystemCpuTimeInSeconds();
-
+        m_startUserCpuTime = getUserCpuTimeInSeconds();
+        m_startSysCpuTime = getSystemCpuTimeInSeconds();
 
         m_descriptionStr = descriptionStr;
-        m_endCpuTime=0;
-        m_sysCpuTime=0;
-        m_sysCpuTime=0;
+        m_endCpuTime = 0;
+        m_sysCpuTime = 0;
+        m_sysCpuTime = 0;
     }
 
     ///@brief    stops the cpu time calculation when this function is called
@@ -305,7 +297,7 @@ enum class Unit_t
     void stop()
     {
         m_endCpuTime = getUserCpuTimeInSeconds() + getSystemCpuTimeInSeconds();
-        std::cout << "CPU Time [ " << m_descriptionStr << " ] : " << m_endCpuTime << " " <<getUnitDesc() << '\n';
+        std::cout << "CPU Time [ " << m_descriptionStr << " ] : " << m_endCpuTime << " " << getUnitDesc() << '\n';
     }
 };
 
@@ -337,9 +329,26 @@ public:
 
 /* ================ End of Metrics measurement code ==================*/
 
+void elapsedTimeDemo()
+{
+    ElapsedTime elapTime;
+    elapTime.start("Demo");
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));
+    elapTime.stop();
 
-// Driver function
-int main()
+    ElapsedTime elapTimeAcc;
+    elapTimeAcc.startAccumulate("Demo Acc");
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));
+    elapTimeAcc.stopAccumulate();
+
+    elapTimeAcc.startAccumulate("Demo Acc");
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));
+    elapTimeAcc.stopAccumulate();
+
+    elapTimeAcc.printTime();
+}
+
+void cpuMetricsDemo()
 {
     CppCpuMetrics cpuMetrics;
     cpuMetrics.start("CpuMetrics");
@@ -353,35 +362,37 @@ int main()
 
     mainPgmMetrics.stop();
 
-
-    ElapsedTime elapTime;
-    elapTime.start("Demo");
-   std::this_thread::sleep_for(std::chrono::microseconds(1000));
-    elapTime.stop();
-
-    ElapsedTime elapTimeAcc;
-    elapTimeAcc.startAccumulate("Demo Acc");
-    std::this_thread::sleep_for(std::chrono::microseconds(1000));
-    elapTimeAcc.stopAccumulate();
-
-    elapTimeAcc.startAccumulate("Demo Acc");
-    std::this_thread::sleep_for(std::chrono::microseconds(1000));
-    elapTimeAcc.stopAccumulate();
-
-    elapTimeAcc.printTime();
-
-
-   
-    int sum=0;
-    std::string s="Hai";
-        for(int i=0;i<1000000;i++)
-        {
-        sum=sum+i;
+    int sum = 0;
+    std::string s = "Hai";
+    for (int i = 0; i < 1000000; i++)
+    {
+        sum = sum + i;
         s += "HELLO";
-        }
+    }
 
     std::cout << sum << '\n';
-            
+
     cpuMetrics.stop();
-    
+}
+
+inline void elapsedTimeWithMacroDemo()
+{
+ELAPSED_TIME_B(L379);
+std::this_thread::sleep_for(std::chrono::microseconds(1000));
+ELAPSED_TIME_E(L379);
+
+
+ELAPSED_TIME_B(L385);
+std::this_thread::sleep_for(std::chrono::microseconds(1000));
+ELAPSED_TIME_E(L385);
+
+}
+
+// Driver function
+int main()
+{
+    elapsedTimeDemo();
+    cpuMetricsDemo();
+    elapsedTimeWithMacroDemo();
+
 }
