9a9b77592b5600c0575b0f7830e77ba7ddbb502b
Vinoth Energetic <vinothcse2000@gmail.com>
2023-02-14 12:28:32 +0530

    Update V6LinuxInit.sh

--- ShellScript/V6LinuxInit.sh
+++ ShellScript/V6LinuxInit.sh
@@ -8,9 +8,15 @@ alias ll='ls -hltr'
 alias ebash='vi /home/nrspapp/v6.bash'
 
 #git
-alias gs='git status'
-alias gb='git branch'
+alias gb='git rev-parse --abbrev-ref HEAD'
+alias gbl='git branch'
 alias gba='git branch -a'
+alias gs='git stash'
+alias gsl='git stash list'
+alias gsa='git stash apply'
+alias gpu='git push origin `git rev-parse --abbrev-ref HEAD`'
+alias gpl='git pull origin `git rev-parse --abbrev-ref HEAD`'
+alias gfa='git fetch --all'
 
 
 #Project Specific
